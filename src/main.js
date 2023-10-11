672d0f63f046ac3479b5814ce53f38f8d5eb8986
Nicolas Lopez <niclopezt@gmail.com>
2023-10-11 18:29:23 +1100

    Create factory function to wrap API functions

--- src/main.js
+++ src/main.js
@@ -64,34 +64,68 @@ const appElements = (() => {
 //     city.value = "";
 // };
 
-weatherUpdate = (location, units = "metric") => {
-    /**
-     * API Key from nico9506 user, Openweathermap
-     * string 'location' --> {city name},{state code},{country code}
-     * string 'units' --> standard, metric and imperial
-     */
-    const xhr = new XMLHttpRequest();
-    xhr.open(
-        "GET",
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=243eecaa621a7c5bbe4b86f7bc268e9e&units=${units}`
-        //URL has to be changed to receive the function parameters
-    );
+const APIUtilities = () => {
+    const updateWeather = (location, units = "metric") => {
+        /**
+         * API Key from nico9506 user, Openweathermap
+         * string 'location' --> {city name},{state code},{country code}
+         * string 'units' --> standard, metric and imperial
+         * Documentation: https://openweathermap.org/current#one
+         */
+        const xhr = new XMLHttpRequest();
+        xhr.open(
+            "GET",
+            `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=243eecaa621a7c5bbe4b86f7bc268e9e&units=${units}`
+            //URL has to be changed to receive the function parameters
+        );
 
-    xhr.send();
-    xhr.onload = () => {
-        if (xhr.status === 404) {
-            console.log("Place not found!");
-        } else {
-            let data = JSON.parse(xhr.response);
-            appElements.setCityName(data.name);
-            appElements.setTemperature(data.main.temp);
-            appElements.setCondition(data.weather[0].main);
-            appElements.setDescription(data.weather[0].description);
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status === 404) {
+                console.log("Place not found!");
+            } else {
+                let data = JSON.parse(xhr.response);
+                searchCity(data.coord.lat, data.coord.lon);
+                appElements.setTemperature(data.main.temp);
+                appElements.setCondition(data.weather[0].main);
+                appElements.setDescription(data.weather[0].description);
 
-            //Image source --- It must be tested
-            //image = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        }
+                //Image source --- It must be tested
+                //image = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            }
+        };
     };
+
+    const searchCity = (lat, lon) => {
+        /**
+         * API Key from nico9506 user, Openweathermap
+         * 'lat' --> latitude
+         * 'lon' --> longitude
+         * Documentation: https://openweathermap.org/api/geocoding-api
+         *
+         * Change the 'location' HTML for a string compound by the city and country names.
+         */
+
+        const xhr = new XMLHttpRequest();
+        xhr.open(
+            "GET",
+            `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=243eecaa621a7c5bbe4b86f7bc268e9e`
+        );
+
+        xhr.send();
+        xhr.onload = () => {
+            if (xhr.status === 404) {
+                console.log("Place not found!");
+            } else {
+                let data = JSON.parse(xhr.response);
+                appElements.setCityName(
+                    String(data[0].name) + ", " + String(data[0].country)
+                );
+            }
+        };
+    };
+
+    return { updateWeather };
 };
 
 printWeather = () => {
